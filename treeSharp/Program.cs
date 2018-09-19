6b9bb69a24c1a3e2120c350555161b43140fc204
iamtheyammer <32084206+iamtheyammer@users.noreply.github.com>
2018-09-19 14:48:54 -0700

    Initial commit - version 0.1
    Contains both initial releases

---
+++ treeSharp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Input;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace treeSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool debug = false;
+            int delay = 0;
+            string hackerTyper = "";
+            //if(System.IO.Directory.GetCurrentDirectory() != "C:\\") return;
+            string treeFile = treeSharp.Properties.Resources.fakeTree;
+            string[] fakeTree = treeFile.Split('\n');
+               /*System.IO.File.ReadAllLines(@"C:\\Users\\George\\Documents\\fakeTree.txt");*/
+
+            for(int i = 0; i < fakeTree.Length; i++)
+            {
+                Console.WriteLine(fakeTree[i]);
+                if (debug && i > 10) break;
+                if (Console.KeyAvailable)
+                {
+                    delay += 25;
+                    hackerTyper = hackerTyper + Console.ReadKey().KeyChar;
+                }
+                Thread.Sleep(delay);
+                if (delay >= 500) break;
+            }
+
+            Console.WriteLine("\n\nSystem clean. No viruses or hackers found.");
+            //Console.WriteLine("")
+           // hackerTyper = Reverse(hackerTyper);
+            //Console.WriteLine("\nC:\\>" + hackerTyper);
+            //Console.WriteLine("\'" + hackerTyper + "\' is not recognized as an internal or external command,\noperable program or batch file.");
+            //Console.ReadKey();
+            return;
+            
+        }
+
+        static void DebugPrint(bool debugBool, string toPrint)
+        {
+            if (debugBool) Console.WriteLine(toPrint);
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
---
+++ treeSharp/resources/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Input;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace treeSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool debug = false;
+            int delay = 0;
+            //if(System.IO.Directory.GetCurrentDirectory() != "C:\\") return;
+            Console.WriteLine("Hello World!");
+            string[] fakeTree = System.IO.File.ReadAllLines(@"C:\\Users\\George\\Documents\\fakeTree.txt");
+
+            for(int i = 0; i < fakeTree.Length; i++)
+            {
+                Console.WriteLine(fakeTree[i]);
+                if (debug && i > 10) break;
+                if (Console.KeyAvailable) delay += 10;
+                if(delay >= 1000)
+                {
+                    break;
+                } else
+                {
+                    while(delayTimer(delay))
+                    {
+                        if (Console.KeyAvailable) delay += 10;
+                    }
+                }
+
+            }
+
+            Console.WriteLine("\nSystem clean. No viruses or hackers found.\n");
+            //Console.ReadKey();
+            return;
+            
+        }
+
+        static void DebugPrint(bool debugBool, string toPrint)
+        {
+            if (debugBool) Console.WriteLine(toPrint);
+        }
+
+        static bool delayTimer(int millis)
+        {
+            Thread.Sleep(millis);
+            return true;
+        }
+    }
+}
